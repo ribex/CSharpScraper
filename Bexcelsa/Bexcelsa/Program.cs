91d3c5c45951d46df6dc5a5da1c7b9e891d3e4c5
Rebecca Wagaman <rebecca.wagaman@gmail.com>
2018-01-07 18:04:31 -0500

    Navigate to SeekingAlpha

--- Bexcelsa/Bexcelsa/Program.cs
+++ Bexcelsa/Bexcelsa/Program.cs
@@ -7,8 +7,8 @@ namespace Bexcelsa
     {
         static void Main(string[] args)
         {
-
             var chromeDriver = new ChromeDriver(@"C:\Users\Rebecca\source\repos\CSharpScraper\Bexcelsa");
+            chromeDriver.Navigate().GoToUrl("https://seekingalpha.com/");
         }
     }
 }
