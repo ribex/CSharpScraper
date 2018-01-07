ced0698fcbc5c280518b2b91a6b6fc8327a0aa7e
Rebecca Wagaman <rebecca.wagaman@gmail.com>
2018-01-07 18:37:16 -0500

    Rename chromeDriver -> chrDr

--- Bexcelsa/Bexcelsa/Program.cs
+++ Bexcelsa/Bexcelsa/Program.cs
@@ -8,12 +8,21 @@ namespace Bexcelsa
     {
         static void Main(string[] args)
         {
-            var chromeDriver = new ChromeDriver(@"C:\Users\Rebecca\source\repos\CSharpScraper\Bexcelsa");
-            chromeDriver.Navigate().GoToUrl("https://seekingalpha.com/");
+            var chrDr = new ChromeDriver(@"C:\Users\Rebecca\source\repos\CSharpScraper\Bexcelsa");
+            chrDr.Navigate().GoToUrl("https://seekingalpha.com/");
 
-            chromeDriver.FindElementByXPath("//*[@id=\"hd-auto\"]").Click();
-            chromeDriver.Keyboard.SendKeys("Amazon");
-            chromeDriver.Keyboard.SendKeys(Keys.Enter);
+//            chromeDriver.FindElementByXPath("//*[@id=\"hd-auto\"]").Click();
+//            chromeDriver.Keyboard.SendKeys("Amazon");
+//            chromeDriver.Keyboard.SendKeys(Keys.Enter);
+
+            // log in to site
+            chrDr.FindElementByXPath("//*[@id=\"sign-in\"]").Click();
+            chrDr.FindElementByXPath("//*[@id=\"authentication_login_email\"]").Click();
+            chrDr.Keyboard.SendKeys("rebecca.wagaman@intracitygeeks.org");
+            chrDr.Keyboard.SendKeys(Keys.Tab);
+            chrDr.Keyboard.SendKeys("ChromeDriver2018");
+            chrDr.Keyboard.SendKeys(Keys.Tab);
+            chrDr.Keyboard.SendKeys(Keys.Enter);
         }
     }
 }
