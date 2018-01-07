31d261c23d3f1f93f68a62d3fc20f231a000920e
Rebecca Wagaman <rebecca.wagaman@gmail.com>
2018-01-07 18:08:39 -0500

    Search for Amazon on SeekingAlpha

--- Bexcelsa/Bexcelsa/Program.cs
+++ Bexcelsa/Bexcelsa/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
 namespace Bexcelsa
@@ -9,6 +10,10 @@ namespace Bexcelsa
         {
             var chromeDriver = new ChromeDriver(@"C:\Users\Rebecca\source\repos\CSharpScraper\Bexcelsa");
             chromeDriver.Navigate().GoToUrl("https://seekingalpha.com/");
+
+            chromeDriver.FindElementByXPath("//*[@id=\"hd-auto\"]").Click();
+            chromeDriver.Keyboard.SendKeys("Amazon");
+            chromeDriver.Keyboard.SendKeys(Keys.Enter);
         }
     }
 }
