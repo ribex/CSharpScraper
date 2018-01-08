a235854131cbb4641bd5131e36ead7f0f60305cf
Rebecca Wagaman <rebecca.wagaman@gmail.com>
2018-01-07 19:24:50 -0500

    List portfolio data in console

--- Bexcelsa/Bexcelsa/Program.cs
+++ Bexcelsa/Bexcelsa/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
@@ -16,13 +17,23 @@ namespace Bexcelsa
 //            chromeDriver.Keyboard.SendKeys(Keys.Enter);
 
             // log in to site
+            // if this fails, site thinks we are using automation to access it... which we are
             chrDr.FindElementByXPath("//*[@id=\"sign-in\"]").Click();
             chrDr.FindElementByXPath("//*[@id=\"authentication_login_email\"]").Click();
             chrDr.Keyboard.SendKeys("rebecca.wagaman@intracitygeeks.org");
             chrDr.Keyboard.SendKeys(Keys.Tab);
             chrDr.Keyboard.SendKeys("ChromeDriver2018");
-            chrDr.Keyboard.SendKeys(Keys.Tab);
-            chrDr.Keyboard.SendKeys(Keys.Enter);
+            chrDr.FindElementByXPath("//*[@id=\"authentication_login_button\"]/input").Click();
+
+            // navigate to Portfolio
+            chrDr.FindElementByXPath("//*[@id=\"nav_my_portfolio_tab\"]/a").Click();
+
+            // get portfolio elements
+            var portfolioList = chrDr.FindElementsByCssSelector("#side_portfolio");
+            foreach (var symbol in portfolioList)
+            {
+                Console.WriteLine(symbol.Text);
+            }
         }
     }
 }
