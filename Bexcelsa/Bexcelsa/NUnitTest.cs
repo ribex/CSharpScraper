51259f8e48016278f0eb31238c1d91e22b6de5ca
Rebecca Wagaman <rebecca.wagaman@gmail.com>
2018-01-16 20:08:58 -0500

    Add NUnit test

---
+++ Bexcelsa/Bexcelsa/NUnitTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+
+namespace Bexcelsa
+{
+    public class NUnitTest
+    {
+        private ChromeDriver _driver;
+
+        [SetUp]
+        public void Initialize()
+        {
+            _driver = new ChromeDriver(@"C:\Users\Rebecca\source\repos\CSharpScraper\Bexcelsa");
+        }
+
+        [Test]
+        public void OpenAppTest()
+        {
+            _driver.Url = "http://www.demoqa.com";
+        }
+
+        [TearDown]
+        public void EndTest()
+        {
+            _driver.Close();
+        }
+    }
+}
