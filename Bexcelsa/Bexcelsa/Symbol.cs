19e9c40b75cc8982d5dbac7850fc8669c8df12ab
Rebecca Wagaman <rebecca.wagaman@gmail.com>
2018-01-20 20:24:16 -0500

    Change Symbol Int32 property types to string for the time being

--- Bexcelsa/Bexcelsa/Symbol.cs
+++ Bexcelsa/Bexcelsa/Symbol.cs
@@ -12,10 +12,13 @@ namespace Bexcelsa
             public double PercentChange { get; set; }
             public string Currency { get; set; }
             public DateTime MarketTime { get; set; }
-            public Int32 Volume { get; set; }
+            // todo: convert Volume from string to Int32
+            public string Volume { get; set; }
             public double Shares { get; set; }
-            public Int32 AvgVol3Mon { get; set; }
-            public Int32 MarketCap { get; set; }
+            // todo: convert AvgVol3Mon from string to Int32
+            public string AvgVol3Mon { get; set; }
+            // todo: convert MarketCap from string to Int32
+            public string MarketCap { get; set; }
         }
     }
 }
